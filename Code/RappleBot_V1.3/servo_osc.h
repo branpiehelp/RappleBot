238060d4b4c4ede449aab946a6edae0d9f12fcae
branpiehelp <78118692+branpiehelp@users.noreply.github.com>
2021-02-18 13:25:01 +0900

    Add files via upload

---
+++ Code/RappleBot_V1.3/servo_osc.h
@@ -0,0 +1,28 @@
+/********************************************//** 
+© 2021 branpie.com <branpiehelp@gmail.com>
+Rapple Bot
+************************************************/
+#ifndef servo_osc_h
+#define servo_osc_h
+#include <Servo.h>
+class servo_osc{
+private:
+    Servo _servo;
+    int pin;
+    int reverse;
+    int c_angle;
+    int t_angle;
+    int trim;
+    int dt;
+public:
+    servo_osc(int trim=0,int rev=0) 
+    {this->trim=trim; this->reverse=rev;};
+	  void attach(int pin);
+    void detach();
+    void servo_osc::write(int angle);
+    bool servo_osc::refresh(int dt);
+    int get_sub_angle();
+    void get_info();
+};
+
+#endif
